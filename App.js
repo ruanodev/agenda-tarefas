8da3d409f017c4d32b7ba8879b7111d1505526ee
Israel <israel.ruano@gmail.com>
2022-04-29 23:06:54 -0300

    atualiza

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ const App = () => {
               </>
              )}
            />
-          <Route path="/agenda-tarefas/:tarefaTitle" exact component={TarefaDetails} />
+          <Route path="https://ruanodev.github.io/agenda-tarefas/:tarefaTitle" exact component={TarefaDetails} />
           
       </div>
       
